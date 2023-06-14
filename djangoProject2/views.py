67c21b48d39785c24297f0c499262a46b7a77cf1
Svet Ivanov <svetoslav.bc@gmail.com>
2023-06-14 18:32:12 +0300

    added templateView

--- djangoProject2/views.py
+++ djangoProject2/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
-
-
-def index(request):
-    return render(request, 'index.html', context={
-        'who': 'Denis — eto moi pervie strochki koda na Django',
-    })
+from django.views.generic.base import TemplateView
 
 
 def about(request):
     return render(request, 'about.html')
+
+
+class HomePageView(TemplateView):
+
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['who'] = 'Denis — eto moi pervie strochki koda na Django'
+        return context
