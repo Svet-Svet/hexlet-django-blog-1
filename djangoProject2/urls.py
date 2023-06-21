1e0989bc5a9394197781ee77c6724ccd242c1fcd
Svet Ivanov <svetoslav.bc@gmail.com>
2023-06-21 19:05:15 +0300

    added templateView_new

--- djangoProject2/urls.py
+++ djangoProject2/urls.py
@@ -17,10 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from djangoProject2 import views
-from django.views.generic.base import TemplateView
+
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html')),
+    path('', views.HomePageView.as_view(template_name='index.html')),
     path('about/', views.about),
     path('articles/', include('djangoProject2.article.urls')),
     path('admin/', admin.site.urls),
